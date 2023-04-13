d45a038a301850ea7255730df147c8ae84ab5511
Daniel Xu <zhiqiang.xu2015@gmail.com>
2023-04-13 16:48:37 -0400

    Upgrade to Angular 15

--- mccss-registration/src/app/app.module.ts
+++ mccss-registration/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CookieService } from 'ngx-cookie';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -38,7 +39,8 @@ import { ErrorInterceptor } from './interceptor/error.interceptor';
   exports: [],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: LanguageInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    CookieService
   ],
   bootstrap: [AppComponent]
 })
